4d7756e20e7052ed6265b258790adb36a8b20c7c
shinelon <syq_sunshine@163.com>
2017-09-27 17:12:06 +0800

    add sql

---
+++ admin.sql
@@ -0,0 +1,83 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.5.40 : Database - admin
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`admin` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `admin`;
+
+/*Table structure for table `sys_role` */
+
+CREATE TABLE `sys_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL COMMENT '角色名称',
+  `status` tinyint(4) DEFAULT NULL COMMENT '状态',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `sys_role` */
+
+insert  into `sys_role`(`id`,`name`,`status`,`update_time`,`create_time`) values (1,'ROLE_ADMIN',1,NULL,'2017-09-28 00:00:00'),(2,'ROLE_USER',1,NULL,'2017-09-28 00:00:00');
+
+/*Table structure for table `sys_user` */
+
+CREATE TABLE `sys_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `username` varchar(20) DEFAULT NULL COMMENT '用户名',
+  `mobile` int(11) NOT NULL COMMENT '手机号码',
+  `password` varchar(32) NOT NULL COMMENT '密码',
+  `email` varchar(32) DEFAULT NULL COMMENT '邮箱地址',
+  `status` tinyint(4) DEFAULT NULL COMMENT '状态',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `sys_user` */
+
+insert  into `sys_user`(`id`,`username`,`mobile`,`password`,`email`,`status`,`update_time`,`create_time`) values (1,'admin',0,'admin',NULL,1,NULL,'2017-09-28 00:00:00'),(2,'abel',0,'abel',NULL,1,NULL,'2017-09-28 00:00:00');
+
+/*Table structure for table `sys_user_role` */
+
+CREATE TABLE `sys_user_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL COMMENT '用户id',
+  `role_id` bigint(20) DEFAULT NULL COMMENT '角色id',
+  `status` tinyint(4) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `sys_user_role` */
+
+insert  into `sys_user_role`(`id`,`user_id`,`role_id`,`status`,`update_time`,`create_time`) values (1,1,1,1,NULL,'2017-09-28 00:00:00'),(2,2,2,1,NULL,'2017-09-28 00:00:00');
+
+/*Table structure for table `test` */
+
+CREATE TABLE `test` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `content` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `test` */
+
+insert  into `test`(`id`,`content`) values (1,'测试1'),(2,'测试2');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
